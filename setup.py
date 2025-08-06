754dfd77ba6843c8dc24a75a164735adcbd76454
Aldric Marshall <klove144@bellsouth.net>
2025-08-06 01:05:37 -0400

    Fix: simplify setup.py to static metadata to avoid build crash

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='inteldove-backend',
     version='0.1.0',
-    packages=[''],
+    description='IntelDove backend service',
+    packages=['.'],
     install_requires=[],
 )
