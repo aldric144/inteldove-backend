d4d77947f0823c3fe7f2bce31828ef3b83206259
Aldric Marshall <klove144@bellsouth.net>
2025-08-06 01:02:06 -0400

    Fix: hardcode version in setup.py to resolve build issue

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
-__version__ = '0.1.0'
-
 from setuptools import setup
 
 setup(
     name='inteldove-backend',
-    version=__version__,
-    description='INTELDOVE Backend',
-    author='Aldric Marshall',
-    packages=[],
+    version='0.1.0',
+    packages=[''],
+    install_requires=[],
 )
